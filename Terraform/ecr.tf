0f3da33a3a12fb53aa8afcf931c59d9c1f959cbb
efreetsultan <krizsan.robert.szv@gmail.com>
2023-06-01 13:26:32 +0200

    modify the backend, the frontend, and the terraform code

--- Terraform/ecr.tf
+++ Terraform/ecr.tf
@@ -1,5 +1,15 @@
-resource "aws_ecr_repository" "example" {
-  name = var.ecr_repository_name
+resource "aws_ecr_repository" "repo1" {
+  name = "${var.ecr_repository_name}_server"
+
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_ecr_repository" "repo2" {
+  name = "${var.ecr_repository_name}_client"
 
   image_tag_mutability = "MUTABLE"
 
