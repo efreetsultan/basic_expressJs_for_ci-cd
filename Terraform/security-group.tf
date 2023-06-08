3d2f663bcb85420cdf1c0febb12ed776439e1190
efreetsultan <krizsan.robert.szv@gmail.com>
2023-06-08 23:44:38 +0200

    modify the security groups of the ec2-s

--- Terraform/security-group.tf
+++ Terraform/security-group.tf
@@ -25,6 +25,13 @@ resource "aws_security_group" "private_instance_sg" {
     protocol    = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
   }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "bastion_sg" {
@@ -36,6 +43,13 @@ resource "aws_security_group" "bastion_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 }
 
